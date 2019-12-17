acc90b12735b688491971ebef94ab01d5109f76b
PedroSpiet <pedro.silva940@etec.sp.gov.br>
2019-12-17 08:24:22 -0300

    Criando Lado A e Lado B

--- jogo.js
+++ jogo.js
@@ -21,7 +21,7 @@
 		// Criar elementos html
 		var mosquito = document.createElement('img')
 		mosquito.src = 'imagens/mosca.png'
-		mosquito.className = tamanhoAleatorio()
+		mosquito.className = tamanhoAleatorio() + ' ' +ladoAleatorio()
 		mosquito.style.left = posicaoX + 'px'
 		mosquito.style.top = posicaoY + 'px'
 		mosquito.style.position = 'absolute'
@@ -47,6 +47,17 @@
 
 	console.log(tamanhoAleatorio())
 
+	function ladoAleatorio(){
+		var classe = Math.floor(Math.random() * 3)
+
+		switch(classe){
+			case 0:
+				return 'ladoA'
+			
+			case 1:
+				return 'ladoB'
+		}
+	}
 
 
 
